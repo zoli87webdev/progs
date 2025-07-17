2e33cf97c5c0a4a8e76588d2bfb1c8befdcee4bb
zoli87webdev <zoli87webdev@gmail.com>
2025-07-17 14:44:12 +0200

    created project & route

---
+++ Laravel_homework/homework_halado_4/laravel-breeze-app/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+Route::middleware('auth:sanctum')->prefix('users')->group(function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('{id}', [UserController::class, 'update']);
+    Route::delete('{id}', [UserController::class, 'destroy']);
+});
